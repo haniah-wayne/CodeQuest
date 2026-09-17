9d64c203a3db2200df123babd31f7c2ab6dd5fdc
louis <louisdeprez5@gmail.com>
2026-09-17 13:49:14 -0400

    hopefully fixed error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,12 @@
 import {createClient} from '@supabase/supabase-js' 
+
+declare const process: {
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: string
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+    }
+}
+
 export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
