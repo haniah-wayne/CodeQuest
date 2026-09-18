50fb72721b4ff977899437fb4125450d6453a951
yanyancai <hy1033@wayne.edu>
2026-09-18 09:40:26 -0400

    update classes.sql

--- database/classes.sql
+++ database/classes.sql
@@ -1,5 +1,32 @@
-CREATE TABLE classes(
-  classesid int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  classesname TEXT NOT NULL,
-  professorid UUID REFERENCES profiles(id)
+CREATE TABLE Classes (
+    ClassID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ProfessorID UUID REFERENCES Profiles(ID),
+    ClassName TEXT NOT NULL,
+    Description TEXT,
+    ClassCode INT NOT NULL,
+    CreatedAt TIMESTAMPTZ DEFAULT NOW()
+);
+
+ALTER TABLE Classes ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY Policyforclasses_Prof ON Classes
+AS PERMISSIVE
+FOR ALL
+TO authenticated
+USING (auth.uid() = ProfessorID)
+WITH CHECK (auth.uid() = ProfessorID);
+
+
+CREATE POLICY Policyforclasses_Student ON Classes
+AS PERMISSIVE
+FOR SELECT
+TO authenticated
+USING (
+    EXISTS (
+        SELECT 1
+        FROM Enrollment
+        WHERE enrollments.StudentID = auth.uid()
+        AND enrollments.classesid = classes.classesid
+    )
 );
