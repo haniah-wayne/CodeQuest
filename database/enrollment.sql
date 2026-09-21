b140b649ea060f4edc6d9cbcde5834b8d34e114d
yanyancai <hy1033@wayne.edu>
2026-09-21 11:16:45 -0400

    Add RLS policies for enrollments table

--- database/enrollment.sql
+++ database/enrollment.sql
@@ -3,3 +3,25 @@ CREATE TABLE enrollments(
   studentid UUID NOT NULL REFERENCES profiles(id),
   classesid int NOT NULL REFERENCES classes(classesid)
 );
+ALTER TABLE enrollments ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY PolicyforenrollmentS ON enrollments
+AS PERMISSIVE
+FOR SELECT
+TO authenticated
+USING (
+    studentid = auth.uid()
+);
+
+CREATE POLICY PolicyforclenrollmentP ON Enrollments
+AS PERMISSIVE
+FOR SELECT
+TO authenticated
+USING (
+    EXISTS (
+        SELECT 1
+        FROM classes
+        WHERE classes.classesid= enrollments.classesid
+        AND classes.professorid = auth.uid()
+    )
+);
