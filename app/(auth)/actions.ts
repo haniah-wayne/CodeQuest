256e4a5908151f30246e61c12cf18b55b09e1c7b
haniah-wayne <hl1263@wayne.edu>
2026-09-23 13:01:44 -0400

    fixing the flow and server functions

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -20,3 +20,39 @@ export async function signUp(
 	// TODO
 	return {} as never;
 }
+
+import { redirect } from "next/navigation";
+//functionality for the forget and reset password forms
+import { createClient } from "@/lib/supabase/client";
+
+export async function sendResetPasswordEmail(prevState: unknown, formData: FormData) {
+	//calls server functiona to send the email, parameter of state and data
+	const supabase = await createClient(); //new client
+
+	const { error } = await supabase.auth.resetPasswordForEmail(
+		//the form data being sent is email as a string
+		formData.get("email") as string,
+	);
+
+	if (error) {
+		//return object describing error
+		return { success: "", error: error.message };
+	}
+
+	return { success: "Please check your email", error: "" };
+}
+
+export async function updatePassword(prevState: unknown, formData: FormData) {
+	//same thing as above but this time formdata is password
+	const supabase = await createClient();
+
+	const { error } = await supabase.auth.updateUser({
+		password: formData.get("password") as string,
+	});
+
+	if (error) {
+		return { success: "", error: error.message };
+	}
+
+	return { success: "Password updated", error: "" };
+}
